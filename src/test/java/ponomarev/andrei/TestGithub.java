2d4ac544e8733cbaea28371a2b6ffa0e53b16712
Андрей <knopikanic@gmail.com>
2022-08-04 13:50:10 +0300

    DZ3_fix

--- src/test/java/ponomarev/andrei/TestGithub.java
+++ src/test/java/ponomarev/andrei/TestGithub.java
@@ -4,7 +4,9 @@ import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
+
 
 public class TestGithub {
 
@@ -15,13 +17,16 @@ public class TestGithub {
     }
 
     @Test
-    void SearchAndTest() {
+    void searchAndTest() {
         open("https://github.com/");
         $("[data-scoped-placeholder=Search]").setValue("selenide").pressEnter();
         $$("ul.repo-list li").first().$("a").click();
         $("#wiki-tab").click();
         $(".markdown-body").shouldHave(text("Soft assertions"));
-        $$("div.markdown-body").first().$("ul").$("li").sibling(4).$("a").click();
+        $(byText("Show 2 more pages…")).scrollTo();
+        $(byText("Show 2 more pages…")).click();
+        $(byText("SoftAssertions")).click();
         $(".markdown-body").shouldHave(text("Using JUnit5 extend test class"));
     }
 }
+
